aeb0c6d56ea5a7cb25cfaa657ef151e2c56d1827
SniperFox213 <dj.gleb@list.ru>
2021-01-02 16:50:33 +0200

    Integrated Vercel

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import sirv from "sirv";
-import Express from "express";
+import express from "express";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-Express()
+const app = express();
+
+app
 	.use(
 		compression({ threshold: 0 }),
 		sirv("static", { dev }),
@@ -15,3 +17,5 @@ Express()
 	.listen(PORT, (err) => {
 		if (err) console.log("error", err);
 	});
+
+export default app;
